0b81c2b942ac4c439c6c70024f1e012f5b086625
Gottfried107 <gottfried.leibniz107@gmail.com>
2023-02-12 16:18:55 +0100

    .

---
+++ elabora.php
@@ -0,0 +1,44 @@
+<?php
+	//recupero con il metodo post il dato inserito,nel campo eta del form
+    $vet_d1=$_POST['vet_d1'];
+	
+    if($vet_d1=="r2d1")
+		echo "Hai risposto correttamente<br>";
+    else
+		echo "NON Hai risposto correttamente<br>";
+		
+    $vet_d2=$_POST['vet_d2'];
+	
+    if($vet_d2=="r4d2")
+		echo "Hai risposto correttamente<br>";
+    else
+		echo "NON Hai risposto correttamente<br>";
+		
+    $vet_d3=$_POST['vet_d3'];
+	
+    if($vet_d3=="r1d3")
+		echo "Hai risposto correttamente<br>";
+    else
+		echo "NON Hai risposto correttamente<br>";
+		 
+    $vet_d4=$_POST['vet_d4'];
+	
+    if($vet_d4=="r2d4")
+		echo "Hai risposto correttamente<br>";
+    else
+		echo "NON Hai risposto correttamente<br>";
+		
+    $vet_d5=$_POST['vet_d5'];
+	
+    if($vet_d5=="r1d5")
+		echo "Hai risposto correttamente<br>";
+    else
+		echo "NON Hai risposto correttamente<br>";
+		
+    $vet_d6=$_POST['vet_d6'];
+	
+    if($vet_d6=="r3d6")
+		echo "Hai risposto correttamente<br>";
+    else
+		echo "NON Hai risposto correttamente<br>";
+?>
